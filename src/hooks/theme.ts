3d51e4db36f769fab4a0e11604e57517de602d31
Lance.Moe <admin@lance.moe>
2023-03-04 01:33:17 +0900

    fix: darkmode

--- src/hooks/theme.ts
+++ src/hooks/theme.ts
@@ -1,19 +1,16 @@
-import { useLocalStorage, useMediaQuery, useUpdateEffect } from 'usehooks-ts';
+import { useEffect } from 'react';
+import { useLocalStorage, useMediaQuery } from 'usehooks-ts';
 
 const COLOR_SCHEME_QUERY = '(prefers-color-scheme: dark)';
 
 // Hook
 export function useDarkMode() {
-  // Use our useLocalStorage hook to persist state through a page refresh.
-  // Read the recipe for this hook to learn more: usehooks.com/useLocalStorage
-  const [enabledState, setEnabledState] = useLocalStorage<boolean | null>('dark-mode-enabled', null);
   // See if user has set a browser or OS preference for dark mode.
   // The usePrefersDarkMode hook composes a useMedia hook (see code below).
   const isDarkOS = useMediaQuery(COLOR_SCHEME_QUERY);
-  // If enabledState is defined use it, otherwise fallback to prefersDarkMode.
-  // This allows user to override OS level setting on our website.
-  const enabled = enabledState === null ? isDarkOS : enabledState;
-  // Fire off effect that add/removes dark mode class
+  // Use our useLocalStorage hook to persist state through a page refresh.
+  // Read the recipe for this hook to learn more: usehooks.com/useLocalStorage
+  const [enabledState, setEnabledState] = useLocalStorage<boolean | null>('dark-mode-enabled', isDarkOS ?? false);
 
   const setThemeColor = (color: string) => {
     const themeMeta = document.head.querySelector('meta[name="theme-color"]');
@@ -26,20 +23,20 @@ export function useDarkMode() {
     document.documentElement.setAttribute('data-theme', theme);
   };
 
-  useUpdateEffect(
+  useEffect(
     () => {
       const className = 'dark';
       const element = window.document.documentElement;
-      setDaisyUiTheme(enabled ? 'mcpdark' : 'mcplight');
-      setThemeColor(enabled ? '#1C4E80' : '#047AFF');
-      if (enabled) {
+      setDaisyUiTheme(enabledState ? 'mcpdark' : 'mcplight');
+      setThemeColor(enabledState ? '#1C4E80' : '#047AFF');
+      if (enabledState) {
         element.classList.add(className);
       } else {
         element.classList.remove(className);
       }
     },
-    [enabled], // Only re-call effect when value changes
+    [enabledState], // Only re-call effect when value changes
   );
   // Return enabled state and setter
-  return [enabled, setEnabledState] as const;
+  return [enabledState, setEnabledState] as const;
 }

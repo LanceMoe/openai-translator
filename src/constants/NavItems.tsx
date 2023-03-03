a0fc1d72581fef757e6e7528e5daf48177b438ec
Lance.Moe <admin@lance.moe>
2023-03-04 05:28:32 +0900

    feat: change navbar icon

--- src/constants/NavItems.tsx
+++ src/constants/NavItems.tsx
@@ -1,8 +1,8 @@
-import { BiBook, BiCustomize } from 'react-icons/bi';
+import { BsGearWideConnected, BsTranslate } from 'react-icons/bs';
 
 export const NAV_ITEMS = [
-  { key: 'translator', label: 'Translator', to: '/', icon: <BiBook size={24} /> },
-  { key: 'config', label: 'Config', to: '/config', icon: <BiCustomize size={24} /> },
+  { key: 'translator', label: 'Translator', to: '/', icon: <BsTranslate size={24} /> },
+  { key: 'config', label: 'Config', to: '/config', icon: <BsGearWideConnected size={24} /> },
 ] as const;
 
 export default NAV_ITEMS;

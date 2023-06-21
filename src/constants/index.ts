9ddc999b2c174e8d9b90b385f1b9eaaccc9575ca
Lance Moe <admin@lance.moe>
2023-06-21 18:49:54 +0900

    feat: add gpt4 0614 model

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,8 +6,10 @@ export const GPT_MODELS = [
   'gpt-3.5-turbo',
   'gpt-4',
   'gpt-4-0314',
+  'gpt-4-0614',
   'gpt-4-32k',
   'gpt-4-32k-0314',
+  'gpt-4-32k-0614',
 ] as const;
 
 export const OPENAI_MODELS = [...GPT_MODELS, 'text-davinci-003', 'text-davinci-002'] as const;
@@ -23,20 +25,24 @@ export const OPENAI_MODELS_TITLES: Record<OpenAIModel, string> = {
   'text-davinci-002': 'text-davinci-002',
   'gpt-4': 'gpt-4 (testing)',
   'gpt-4-0314': 'gpt-4-0314 (testing)',
+  'gpt-4-0614': 'GPT-4 0614 (testing)',
   'gpt-4-32k': 'gpt-4-32k (testing)',
   'gpt-4-32k-0314': 'gpt-4-32k-0314 (testing)',
+  'gpt-4-32k-0614': 'gpt-4-32k-0614 (testing)',
 } as const;
 
 export const OPENAI_MODELS_DESCRIPTION: Record<OpenAIModel, string> = {
-  'gpt-3.5-turbo-0613': 'gpt-3.5-turbo-0613 (30.1B)',
-  'gpt-3.5-turbo-0301': 'GPT-3.5 Turbo 0301 (30.1B)',
-  'gpt-3.5-turbo': 'GPT-3.5 Turbo (30.1B)',
-  'text-davinci-003': 'Text Davinci (3.3B)',
-  'text-davinci-002': 'Text Davinci (3.3B)',
+  'gpt-3.5-turbo-0613': 'GPT-3.5 Turbo 0613',
+  'gpt-3.5-turbo-0301': 'GPT-3.5 Turbo 0301',
+  'gpt-3.5-turbo': 'GPT-3.5 Turbo',
+  'text-davinci-003': 'Text Davinci 003',
+  'text-davinci-002': 'Text Davinci 002',
   'gpt-4': 'GPT-4',
   'gpt-4-0314': 'GPT-4 0314',
+  'gpt-4-0614': 'GPT-4 0614',
   'gpt-4-32k': 'GPT-4 32K',
   'gpt-4-32k-0314': 'GPT-4 32K 0314',
+  'gpt-4-32k-0614': 'GPT-4 32K 0614',
 } as const;
 
 export const LANGUAGES = {

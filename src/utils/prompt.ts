33e84e0fc344bb7d6dceb5509e233b685daa77aa
Jason Huang <jason840507@gmail.com>
2024-05-27 18:13:59 +0800

    feat: add zh-TW UI Locale and improve prompts (#26)
    * feat: add zh-TW UI Locale and improve prompts
    
    - Add Traditional Chinese (zh-TW) UI locale.
    - Modify prompts for more accurate TC translations.
    
    * feat: use lang code badge instead of flag
    
    ---------
    
    Co-authored-by: Lance Moe <admin@lance.moe>

--- src/utils/prompt.ts
+++ src/utils/prompt.ts
@@ -3,19 +3,19 @@ import { Language, LANGUAGES } from '@/constants';
 export const getTranslatePrompt = (fromLang: Language, toLang: Language) => {
   if (fromLang === 'wyw' || fromLang === 'zh-Hans' || fromLang === 'zh-Hant') {
     if (toLang === 'zh-Hant') {
-      return '翻译成繁体白话文';
+      return '翻譯成繁體白話文，使用台灣用語';
     } else if (toLang === 'zh-Hans') {
       return '翻译成简体白话文';
     } else if (toLang === 'yue') {
-      return '翻译成粤语白话文';
+      return '翻譯成粵語白話文';
     }
   }
   if (toLang === 'wyw' || toLang === 'yue') {
-    return `翻译成${LANGUAGES[toLang] || toLang}`;
+    return `翻譯成${LANGUAGES[toLang] || toLang}`;
   }
   if (fromLang === 'auto') {
     if (toLang === 'zh-Hant') {
-      return '翻譯為繁體';
+      return '翻譯為繁體中文，使用台灣用語';
     }
     if (toLang === 'zh-Hans') {
       return '翻译成简体';
@@ -26,8 +26,10 @@ export const getTranslatePrompt = (fromLang: Language, toLang: Language) => {
     return `translate into ${LANGUAGES[toLang] || toLang}`;
   }
   if (fromLang === toLang) {
-    if (toLang === 'zh-Hant' || toLang === 'zh-Hans') {
+    if (toLang === 'zh-Hans') {
       return '润色此句';
+    } else if (toLang === 'zh-Hant') {
+      return '把這段文字潤飾一下，使用台灣用語';
     } else if (toLang === 'ja') {
       return 'この文を磨く';
     } else if (toLang === 'ko') {
